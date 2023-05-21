c07778be0d14ab6e14e551cfd08da8508ead43b9
Niimphu <nimphu@outlook.com>
2023-05-21 23:47:39 +0200

    fixed norm

--- lib_me42.h
+++ lib_me42.h
@@ -6,7 +6,7 @@
 /*   By: yiwong <yiwong@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 12:21:16 by yiwong            #+#    #+#             */
-/*   Updated: 2023/05/20 20:20:11 by yiwong           ###   ########.fr       */
+/*   Updated: 2023/05/21 17:58:30 by yiwong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,9 @@
 
 void	free_pointer(char *str);
 void	free_ppointer(char **str);
+
 int		min(int a, int b);
 int		max(int a, int b);
+int		abs(int n);
 
 #endif
